ea5bf5ab0bdb3b57d3835c8b96fecccd8fb79bc9
Tarunmeena0901 <meenatarun0901@gmail.com>
2024-10-15 01:17:30 +0530

    Implemented binance private connector and types ( my eyes are hurting right now )

--- Binance (Tarun)/types.ts
+++ Binance (Tarun)/types.ts
@@ -29,4 +29,153 @@ export interface Ticker {
     timestamp: number;           
 }
 
+export interface Credential {
+    key: string,
+    secret: string
+}
+
+interface OrderEvent {
+    symbol: string;
+    connectorType: 'Mexc'; 
+    event: string; 
+    state: string;  
+    orderId: number;   
+    sklOrderId: string; 
+    side: string;       
+    price: number;
+    size: string;           
+    notional: number;      
+    filled_price: number;     
+    filled_size: number;
+    timestamp: number;         
+}
+
 export type SklEvent = 'TopOfBook' | 'Trade' | 'Ticker' | 'Ping';
+
+export type AllActiveOrder = {
+    symbol: string;
+    orderId: number;
+    orderListId: number;
+    clientOrderId: string;
+    price: string;
+    origQty: string;
+    executedQty: string;
+    cummulativeQuoteQty: string;
+    status: string;
+    timeInForce: string;
+    type: string;
+    side: string;
+    stopPrice: string;
+    icebergQty: string;
+    time: number;
+    updateTime: number;
+    isWorking: boolean;
+    origQuoteOrderQty: string;
+    workingTime: number;
+    selfTradePreventionMode: string;
+}[];
+
+export type AccountInfo = {
+    makerCommission: number;
+    takerCommission: number;
+    buyerCommission: number;
+    sellerCommission: number;
+    commissionRates: {
+        maker: string;
+        taker: string;
+        buyer: string;
+        seller: string;
+    };
+    canTrade: boolean;
+    canWithdraw: boolean;
+    canDeposit: boolean;
+    brokered: boolean;
+    requireSelfTradePrevention: boolean;
+    preventSor: boolean;
+    updateTime: number;
+    accountType: string;
+    balances: Array<{
+        asset: string;
+        free: string;
+        locked: string;
+    }>;
+    permissions: string[];
+    uid: number;
+};
+
+export interface BinanceOrderProgress {
+    e: "executionReport",
+    s: string; // this.sklSymbol
+    S: string       // Side           
+    i: number;                 // order.d.i (Order ID)
+    c: string;              // order.d.c (Client Order ID)
+    p: number;                   // parseFloat(order.d.p) (Order price)
+    q: number;                    // order.d.v (Order quantity)            
+    z: number;            // parseFloat(order.d.ap) (Average filled price)
+    Z: number;             // parseFloat(order.d.a) (Filled quantity)
+    E: number;              // parseInt(order.t) (Eent time)
+    x: string;
+    X: string;
+    o: string; // order type              
+}
+
+export interface OrderAckResponse {
+    symbol: string;
+    orderId: number;
+    orderListId: number;
+    clientOrderId: string;
+    transactTime: number;
+}
+
+export interface OrderResultResponse {
+    symbol: string;
+    orderId: number;
+    orderListId: number;
+    clientOrderId: string;
+    transactTime: number;
+    price: string;
+    origQty: string;
+    executedQty: string;
+    cummulativeQuoteQty: string;
+    status: string;
+    timeInForce: string;
+    type: string;
+    side: string;
+    workingTime: number;
+    selfTradePreventionMode: string;
+}
+
+export interface OrderFullResponse extends OrderResultResponse {
+    fills: {
+        price: string;
+        qty: string;
+        commission: string;
+        commissionAsset: string;
+        tradeId: number;
+    }[];
+}
+
+export interface CancelSingleOrderData {
+    symbol: string;                       
+    orderId?: number;                  
+    origClientOrderId?: string;  
+    newClientOrderId?: string;   
+    cancelRestrictions?: 'ONLY_NEW' | 'ONLY_PARTIALLY_FILLED'; 
+    recvWindow?: number;          
+    timestamp: number; 
+}
+
+export type placeOrderData = {
+    symbol: string;
+    side: 'BUY' | 'SELL';
+    type: 'LIMIT' | 'MARKET' | 'STOP_LIMIT' | 'TAKE_PROFIT_LIMIT' | 'LIMIT_MAKER';
+    timeInForce?: 'GTC' | 'IOC' | 'FOK';
+    quantity: number;
+    price: number;
+    stopPrice?: number;
+    newClientOrderId?: string;
+    icebergQty?: number;
+    newOrderRespType?: 'ACK' | 'RESULT' | 'FULL';
+    recvWindow?: number;
+    timestamp?: number;
+};
