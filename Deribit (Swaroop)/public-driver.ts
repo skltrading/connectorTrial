2af97b86261df14ea9cf4a0f84f808d8ba568804
5war00p <maddisaiswaroop@gmail.com>
2024-10-15 21:18:16 +0530

    feat: update driver with function calling

--- Deribit (Swaroop)/public-driver.ts
+++ Deribit (Swaroop)/public-driver.ts
@@ -2,7 +2,7 @@ import * as Types from "./types"
 import { DeribitSpotPublicConnector } from './deribit-spot-public-connector';
 
 // Create an instance of the connector
-const connector = new DeribitSpotPublicConnector();
+const connector = new DeribitSpotPublicConnector("BTC-PERPETUAL", { interval: "100ms" });
 
 // Define a message handler
 const handleMessage = (messages: Types.Serializable[]) => {
@@ -10,10 +10,19 @@ const handleMessage = (messages: Types.Serializable[]) => {
 };
 
 // Connect to the WebSocket
-connector.connect(handleMessage).then((success) => {
-    console.log('Connected:', success);
+connector.connect(handleMessage).then(() => {
+    console.log('Connected');
 
-
+    // Simulation
+    setTimeout(() => {
+        // Get version of Deribit API
+        connector.getVersion()
+        
+        setTimeout(() => {
+            // Unsubscribe all (Ticker, Trade, OrderBook) after 5seconds
+            connector.unsubscribeToAllChannels()
+        }, 5000)
+    }, 5000)
 }).catch((error) => {
     console.error('Failed to connect:', error);
 });
