7dd0fed6f814ef1cb9e00244277e6bda1cf022bb
5war00p <maddisaiswaroop@gmail.com>
2024-10-15 21:18:26 +0530

    feat: update driver with function calling for private connector

--- Deribit (Swaroop)/private-driver.ts
+++ Deribit (Swaroop)/private-driver.ts
@@ -2,7 +2,7 @@ import * as Types from "./types"
 import { DeribitSpotPrivateConnector } from './deribit-spot-private-connector';
 
 // Create an instance of the connector
-const connector = new DeribitSpotPrivateConnector();
+const connector = new DeribitSpotPrivateConnector("ETH-PERPETUAL", { interval: "100ms" });
 
 // Define a message handler
 const handleMessage = (messages: Types.Serializable[]) => {
@@ -10,9 +10,21 @@ const handleMessage = (messages: Types.Serializable[]) => {
 };
 
 // Connect to the WebSocket
-connector.connect(handleMessage).then((success) => {
-    console.log('Connected:', success);
+connector.connect(handleMessage).then(() => {
+    console.log('Connected');
 
+    // Same Simulation as Public connector
+    setTimeout(() => {
+        // Get version of Deribit API
+        connector.getVersion()
+        
+        setTimeout(() => {
+            // Unsubscribe all (Ticker, Trade, OrderBook) after 5seconds
+            connector.unsubscribeToAllChannels()
+            connector.requestCurrentActiveOrders()
+            connector.requestAllBalances()
+        }, 5000)
+    }, 5000)
 
 }).catch((error) => {
     console.error('Failed to connect:', error);
